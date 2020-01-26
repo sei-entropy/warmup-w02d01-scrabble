a70c93963a99aec6d19c9df7cef00874a3ebdec7
maimonah95 <mim95s@hotmail.com>
2020-01-26 09:28:28 +0300

    add the letter in object

---
+++ prog.js
@@ -0,0 +1,13 @@
+const scar = function (word) {
+    let scorVal = 0;
+    const letterVal = {
+        1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+        2: ['D', 'G'],
+        3: ['B', 'C', 'M', 'P'],
+        4: ['F', 'H', 'V', 'W', 'Y'],
+        5: ['K'],
+        8: ['J', 'X'],
+        10: ['Q', 'Z']
+    }
+
+}
