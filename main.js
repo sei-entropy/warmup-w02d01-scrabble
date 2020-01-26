4b8d4e91583a1cd39c2f0a9988869ed8176c4ccc
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-01-26 09:21:35 +0300

    Done warmup

---
+++ main.js
@@ -0,0 +1,37 @@
+const scrabble = function(word) {
+    const scrabblePoints = {
+        A: 1, 
+        B: 3,
+        C: 3, 
+        D: 2,
+        E: 1, 
+        F: 4, 
+        G: 2, 
+        H: 4, 
+        I: 1, 
+        M: 3, 
+        N: 1, 
+        O: 1, 
+        P: 3, 
+        Q: 10, 
+        R: 1, 
+        S: 1, 
+        T: 1, 
+        U: 1,
+        V: 4, 
+        W: 4, 
+        X: 8, 
+        Y: 4, 
+        Z: 10,
+    };
+
+    word = word.toUpperCase();
+    let points = 0; 
+    for(let i = 0; i < word.length; i++){
+
+        points += scrabblePoints[word[i]]
+    } 
+
+    return points 
+}; 
+console.log(scrabble('cabbage'));
